cce74ebdae3b0be3fd950edc37d7682b89bd6a20
Mitchell <Hmitchell.hall@gmail.com>
2021-07-27 17:31:10 -0500

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const removeMain = document.getElementById("main")
+removeMain.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id','victory')
+
+const text = document.createTextNode('Mitchell is the champion');
+newHeader.appendChild(text);
